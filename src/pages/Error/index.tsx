f5cc8b31c43e1102c2ec0ad7b2d36346017137f5
Yan Bein <yanbeindev@gmail.com>
2024-07-29 17:22:16 -0300

    feat(core): style error page

--- src/pages/Error/index.tsx
+++ src/pages/Error/index.tsx
@@ -1,11 +1,19 @@
-import { useRouteError } from "react-router-dom";
+import { Alert, Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 export default function ErrorPage() {
-  const error = useRouteError();
-
-  console.error(error);
-
   return (
-    <p>Something went wrong</p>
+    <Container className="mt-5">
+      <Alert variant="danger">
+        <Alert.Heading>Something Went Wrong</Alert.Heading>
+        <p>
+          An unexpected error has occurred. Please try again later or contact support if the issue persists.
+        </p>
+        <hr />
+        <div className="d-flex justify-content-end">
+          <Link to="/">Go to Home</Link>
+        </div>
+      </Alert>
+    </Container>
   )
 }
